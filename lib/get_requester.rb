6d6ed704f862dca57821f0a8adaf4ec767499a86
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-23 20:53:03 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,12 +5,8 @@ require 'json'
 
 class GetRequester
 
-  URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
-
-  def initialize(URL)
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
+  def initialize()
+  
   end
 
   def get_response_body
